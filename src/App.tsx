15c3f58c0d09e70f199e66f5d14d2147140cea1e
andregustavo1 <andre@andregu.com.br>
2026-09-13 20:26:20 -0300

    front end inicial e backend enviando o arquivo para o supabase

--- src/App.tsx
+++ src/App.tsx
@@ -1,121 +1,42 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import './App.css'
+import { RiBookLine, RiUploadLine } from 'react-icons/ri'
+import { supabase } from './lib/supabase'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+async function PdfUpload(file: File) {
+  await supabase.storage
+    .from('pdfs')
+    .upload(file.name, file)
+}
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <div className="flex min-h-screen items-center justify-center bg-[#161515] text-white">
+      <div className="flex flex-col items-center px-4 text-center">
 
-      <div className="ticks"></div>
+      <div className="h-16 w-16 mb-6 flex items-center justify-center text-slate-400">
+        <RiBookLine className="w-full h-full" />
+      </div>
 
-      <section id="next-steps">
-        <div className="bg-red-500" id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+        <h1 className="text-4xl font-bold">E-To-Speech</h1>
+        <p className="mb-6 mt-2 text-base text-slate-400">Importe seus eBooks em PDF e comece a ouvir!</p>
+
+        {/* botão principal */}
+        <label className="flex items-center gap-2 rounded-lg bg-blue-500 px-8 py-2 cursor-pointer" htmlFor="pdf-upload">
+          <div className="flex h-5 w-5 items-center justify-center">
+            <RiUploadLine />
+          </div>
+
+          <p className="">Importar PDF</p>
+        </label>
+
+        <input className="sr-only" id="pdf-upload" type="file" accept="application/pdf" 
+          onChange={(e) => { 
+            const file = e.target.files?.[0]
+
+          if (file) PdfUpload(file)
+        }} />
+      </div>
+    </div>
   )
 }
 
